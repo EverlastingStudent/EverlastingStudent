7de0dd80717b505650f9a8fb0fe8ab884f15d9d5
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-04-05 21:42:17 +0300

    Added DeletableEntityRepository closes #6

---
+++ Source/EverlastingStudent/EverlastingStudent.Data/Repositories/Contracts/IDeletableEntityRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace EverlastingStudent.Data.Repositories.Contracts
+{
+    using System.Linq;
+
+    public interface IDeletableEntityRepository<T> : IGenericRepository<T> where T : class
+    {
+        IQueryable<T> AllWithDeleted();
+    }
+}
