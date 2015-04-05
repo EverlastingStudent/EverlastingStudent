52ead09a2563a71e8e66f77723ca13a65d1795ab
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-04-05 21:41:42 +0300

    Added DeletableEntity model in common project

---
+++ Source/EverlastingStudent/EverlastingStudent.Common/Models/DeletableEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace EverlastingStudent.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using EverlastingStudent.Common.Contracts;
+
+    public abstract class DeletableEntity : IDeletableEntity
+    {
+        [Editable(false)]
+        public bool IsDeleted { get; set; }
+
+        [Editable(false)]
+        public DateTime? DeletedOn { get; set; }
+    }
+}
