61945cd3a5b97f124aaf73dfdfa9e76eba48476f
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-04-05 19:31:38 +0300

    Added Data project with context

---
+++ Source/EverlastingStudent/EverlastingStudent.Data/EverlastingStudentDbContext.cs
@@ -0,0 +1,47 @@
+﻿namespace EverlastingStudent.Data
+{
+    using System.Data.Entity;
+
+    using EverlastingStudent.Models;
+
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class EverlastingStudentDbContext : IdentityDbContext<Student>, IEverlastingStudentDbContext
+    {
+        public EverlastingStudentDbContext()
+            : base("EverlastingStudent", throwIfV1Schema: false)
+        {
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<EverlastingStudentDbContext, DefaultConfiguration>());
+        }
+
+        public IDbSet<Course> Courses { get; set; }
+
+        public IDbSet<Difficulty> Difficulties { get; set; }
+
+        public IDbSet<Drink> Drinks { get; set; }
+
+        public IDbSet<Exam> Exams { get; set; }
+
+        public IDbSet<FreelanceProject> FreelanceProjects { get; set; }
+
+        public IDbSet<HardwarePart> HardwareParts { get; set; }
+
+        public IDbSet<Homework> Homeworks { get; set; }
+
+        public IDbSet<SpecializedCourse> SpecializedCourses { get; set; }
+
+        public IDbSet<StudentFreelanceProject> StudentFreelanceProjects { get; set; }
+
+        public IDbSet<StudentHomework> StudentHomeworks { get; set; }
+
+        public static EverlastingStudentDbContext Create()
+        {
+            return new EverlastingStudentDbContext();
+        }
+
+        public new IDbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+    }
+}
---
+++ Source/EverlastingStudent/EverlastingStudent.Data/IEverlastingStudentDbContext.cs
@@ -0,0 +1,37 @@
+﻿namespace EverlastingStudent.Data
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    using EverlastingStudent.Models;
+
+    public interface IEverlastingStudentDbContext
+    {
+
+        IDbSet<Course> Courses { get; set; }
+
+        IDbSet<Difficulty> Difficulties { get; set; }
+
+        IDbSet<Drink> Drinks { get; set; }
+
+        IDbSet<Exam> Exams { get; set; }
+
+        IDbSet<FreelanceProject> FreelanceProjects { get; set; }
+
+        IDbSet<HardwarePart> HardwareParts { get; set; }
+
+        IDbSet<Homework> Homeworks { get; set; }
+
+        IDbSet<SpecializedCourse> SpecializedCourses { get; set; }
+
+        IDbSet<StudentFreelanceProject> StudentFreelanceProjects { get; set; }
+
+        IDbSet<StudentHomework> StudentHomeworks { get; set; }
+
+        IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        int SaveChanges();
+    }
+}
