396031c5b3054222ffa4f7320f5d85e5b2afb724
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-04-08 22:51:24 +0300

    Added Homework models to seed method #4

--- Source/EverlastingStudent/EverlastingStudent.Data/Migrations/DefaultMigrationConfiguration.cs
+++ Source/EverlastingStudent/EverlastingStudent.Data/Migrations/DefaultMigrationConfiguration.cs
@@ -2,6 +2,9 @@ namespace EverlastingStudent.Data.Migrations
 {
     using System.Data.Entity.Migrations;
 
+    using EverlastingStudent.Common.Models;
+    using EverlastingStudent.Models;
+
     internal sealed class DefaultMigrationConfiguration : DbMigrationsConfiguration<EverlastingStudentDbContext>
     {
         public DefaultMigrationConfiguration()
@@ -12,18 +15,103 @@ namespace EverlastingStudent.Data.Migrations
 
         protected override void Seed(EverlastingStudentDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            this.SeedHomeworks(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private void SeedHomeworks(EverlastingStudentDbContext context)
+        {
+            context.Homeworks.Add(new Homework
+            {
+                Title = "JavaScript Syntax (If statemants, operations, variables...)",
+                Type = TypeOfDifficulty.Easy
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "Java Syntax (If statemants, operations, variables...)",
+                Type = TypeOfDifficulty.Easy
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "PHP Syntax (If statemants, operations, variables...)",
+                Type = TypeOfDifficulty.Medium
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "C# Syntax (If statemants, operations, variables...)",
+                Type = TypeOfDifficulty.Hard
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "JavaScript Loops (for, forin, while...)",
+                Type = TypeOfDifficulty.Easy
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "JavaScript OOP (classical, prototype)",
+                Type = TypeOfDifficulty.Medium
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "JavaScript Functions",
+                Type = TypeOfDifficulty.Hard
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "HTML Talbes",
+                Type = TypeOfDifficulty.Easy
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "Responsive Design",
+                Type = TypeOfDifficulty.Hard
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "SASS, LESS, Jekyll...",
+                Type = TypeOfDifficulty.Medium
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "CSS Selectors",
+                Type = TypeOfDifficulty.Medium
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "Regular Expressions (build in functions)",
+                Type = TypeOfDifficulty.Medium
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "PHP - Apache (server setup, XAMPP, LAMPP, MAMPP)",
+                Type = TypeOfDifficulty.Easy
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "Java Object Oriented Programming",
+                Type = TypeOfDifficulty.Hard
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "DataBases - SQL (Structured  Query Language)",
+                Type = TypeOfDifficulty.Easy
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "Database (SELECT, UPDATE, INSERT, DELETE)",
+                Type = TypeOfDifficulty.Medium
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "High Quality code - Design Patterns",
+                Type = TypeOfDifficulty.Hard
+            });
+            context.Homeworks.Add(new Homework
+            {
+                Title = "High Quality code - SOLID",
+                Type = TypeOfDifficulty.Hard
+            });
+
+            context.SaveChanges();
         }
     }
 }
