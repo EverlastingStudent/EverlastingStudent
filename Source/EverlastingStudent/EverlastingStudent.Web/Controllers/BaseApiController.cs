cf8447298d8c9b8b97a6feb9dd28be410e19f724
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-04-08 22:53:38 +0300

    Added BaseApiController and HomeworksController, Added empty DTO, for GetHomeworks action

---
+++ Source/EverlastingStudent/EverlastingStudent.Web/Controllers/BaseApiController.cs
@@ -0,0 +1,23 @@
+﻿namespace EverlastingStudent.Web.Controllers
+{
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+
+    using EverlastingStudent.Data;
+    using EverlastingStudent.Models;
+
+    public class BaseApiController : ApiController
+    {
+        public BaseApiController(IEverlastingStudentData data)
+        {
+            this.Data = data;
+            this.UserProfile = this.User;
+        }
+
+        protected IEverlastingStudentData Data { get; set; }
+
+        protected Student UserProfile { get; set; }
+
+        
+    }
+}
---
+++ Source/EverlastingStudent/EverlastingStudent.Web/Controllers/HomeworksController.cs
@@ -0,0 +1,13 @@
+﻿namespace EverlastingStudent.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeworksController : BaseApiController
+    {
+        // GET: Homeworks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
