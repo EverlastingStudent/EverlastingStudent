01f3b859aa1ad4b5571d4c37998a6036a4dc04ee
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-04-10 18:55:25 +0300

    Fixed bug in userIdProvider

--- Source/EverlastingStudent/EverlastingStudent.Web/Controllers/BaseApiController.cs
+++ Source/EverlastingStudent/EverlastingStudent.Web/Controllers/BaseApiController.cs
@@ -1,28 +1,29 @@
 ﻿namespace EverlastingStudent.Web.Controllers
 {
-    using System.Net.Http;
-    using System.Threading;
-    using System.Threading.Tasks;
     using System.Web.Http;
-    using System.Web.Http.Controllers;
-    using System.Web.Http.Results;
 
     using EverlastingStudent.Common.Infrastructure;
     using EverlastingStudent.Data;
     using EverlastingStudent.Models;
 
-    using Microsoft.AspNet.Identity;
-
     public class BaseApiController : ApiController
     {
+        private IUserProvider provider;
+
         public BaseApiController(IEverlastingStudentData data, IUserProvider userProvider)
         {
             this.Data = data;
-            this.UserProfile = this.Data.Students.GetById(userProvider.GetUserId());
+            this.provider = userProvider;
         }
 
         protected IEverlastingStudentData Data { get; set; }
 
-        protected Student UserProfile { get; set; }
+        protected Student UserProfile
+        {
+            get
+            {
+                return this.Data.Students.GetById(this.provider.GetUserId());
+            }
+        }
     }
 }
