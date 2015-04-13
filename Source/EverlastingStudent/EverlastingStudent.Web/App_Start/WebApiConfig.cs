fd7edfbb41d543974c56d121730a50d397098d0f
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-04-13 11:50:26 +0300

    Enabled cors and test it with simple js client #18

--- Source/EverlastingStudent/EverlastingStudent.Web/App_Start/WebApiConfig.cs
+++ Source/EverlastingStudent/EverlastingStudent.Web/App_Start/WebApiConfig.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
-
-namespace EverlastingStudent.Web
+﻿namespace EverlastingStudent.Web
 {
+    using System.Web.Http;
+
+    using Microsoft.Owin.Security.OAuth;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -16,6 +12,7 @@ namespace EverlastingStudent.Web
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
