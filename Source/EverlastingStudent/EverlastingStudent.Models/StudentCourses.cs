426a0490aa1271a7f3884e0886bf018690bcc483
Tsvetina Georgieva <tsv.georgieva@abv.bg>
2015-04-16 08:23:39 +0300

    added more missing files

---
+++ Source/EverlastingStudent/EverlastingStudent.Models/StudentCourses.cs
@@ -0,0 +1,29 @@
+﻿namespace EverlastingStudent.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class StudentCourses
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string StudentId { get; set; }
+
+        public virtual Student Student { get; set; }
+
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool ExamInProgress { get; set; }
+
+        public bool IsPassed { get; set; }
+
+        public bool IsPassedWithExcellence { get; set; }
+
+        public DateTime? PassedOn { get; set; }
+    }
+}
---
+++ Source/EverlastingStudent/EverlastingStudent.Models/StudentLectures.cs
@@ -0,0 +1,25 @@
+﻿namespace EverlastingStudent.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class StudentLectures
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string StudentId { get; set; }
+
+        public virtual Student Student { get; set; }
+
+        public int LectureId { get; set; }
+
+        public virtual Lecture Lecture { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsPassed { get; set; }
+
+        public DateTime? PassedOn { get; set; }
+    }
+}
